6e205b5e2d809ac708f3d51627f2bdb0197da5e3
Timothy Osmond <45649121+timothyosm@users.noreply.github.com>
2021-05-09 17:33:25 +0800

    added test to test unique

--- App/src/Util/colorGen.test.js
+++ App/src/Util/colorGen.test.js
@@ -4,3 +4,11 @@ test("Should output 32768 colors", () => {
   const colors = generateColors(8);
   expect(colors).toHaveLength(32768);
 });
+
+test("All Values must be unique", () => {
+  const colors = generateColors(8);
+  const isArrayUnique = (arr) =>
+    Array.isArray(arr) && new Set(arr).size === arr.length; // add function to check that array is unique.
+
+  expect(isArrayUnique(colors)).toBeTruthy();
+});
--- App/src/Util/luminocitySort.test.js
+++ App/src/Util/luminocitySort.test.js
@@ -6,3 +6,11 @@ test("Should output 32768 colors", () => {
   const sortedArray = luminocitySort(rgbArray);
   expect(sortedArray).toHaveLength(32768);
 });
+
+test("All Values must be unique", () => {
+  const colors = generateColors(8);
+  const isArrayUnique = (arr) =>
+    Array.isArray(arr) && new Set(arr).size === arr.length; // add function to check that array is unique.
+
+  expect(isArrayUnique(colors)).toBeTruthy();
+});
